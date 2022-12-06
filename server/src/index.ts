cace462eb48d105bb0e7b663a43320bc0881fa79
ameer <ameer.em.89@gmail.com>
2022-12-06 21:18:18 +0200

    add change in index.ts for deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://vacations-project-api.onrender.com",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
